6bc7f6945f33a281b2af29ecffbdda4db7c8146f
Bárbara Stella <barbarastellaw@gmail.com>
2026-06-19 10:35:22 -0300

    feat: implement google oauth for web and android

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'firebase_options.dart';
+import 'features/auth/bloc/auth_bloc.dart';
+import 'features/auth/bloc/auth_event.dart';
+import 'features/auth/pages/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const ElibraryApp());
@@ -16,7 +25,17 @@ class ElibraryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [],
+      providers: [
+        BlocProvider<AuthBloc>(
+        create: (context) {
+          final webClientId = dotenv.env['GOOGLE_WEB_CLIENT_ID'] ?? '';
+
+          final bloc = AuthBloc(firebaseAuth: FirebaseAuth.instance, googleSignIn: GoogleSignIn(clientId: kIsWeb ? webClientId : null));
+          bloc.add(const AuthCheckRequestedEvent());
+          return bloc;
+        },
+    ),
+      ],
       child: MaterialApp(
         title: 'eLibrary App',
         debugShowCheckedModeBanner: false,
@@ -24,12 +43,7 @@ class ElibraryApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const Scaffold(
-          body: Center(
-            child: Text('eLibrary App\nFirebase e BloC inicializados', textAlign: TextAlign.center, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
+        home: const LoginPage(),
       ),
     );
   }
